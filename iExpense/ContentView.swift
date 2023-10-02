99e8b7f550b4503cc048a6d05471deaf5961280e
Chuck Belcher <cfbelcher@pateogroup.com>
2023-10-01 20:26:37 -0400

    Code pushed this time.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(expenses.items, id: \.name) { item in
+                        Text(item.name)
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                .toolbar {
+                    Button {
+                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                        expenses.items.append(expense)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+                
+            }
+            .navigationTitle("iExpense")
         }
-        .padding()
+        
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
